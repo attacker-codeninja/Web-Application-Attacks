de5207e5bd948eec69432cac9a9f2b85e8dd7e3d
Joshua Franco <francoswim@yahoo.com>
2023-06-09 14:36:35 -0400

    adding registration functions, adding ability to create mysql database for users

--- Broken_Access_Control/IDOR/app.py
+++ Broken_Access_Control/IDOR/app.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
+
+# Connect to the database
+db = mysql.connector.connect(
+    host='db',
+    user='myuser',
+    password='mypassword',
+    database='mydatabase'
+)
+# Create a table
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        password VARCHAR(255)
+    )
+''')
+
+# Commit the changes and close the connection
+db.commit()
+db.close()
 
 @app.route('/')
 def home():
@@ -12,6 +35,21 @@ def login():
 
 @app.route('/register')
 def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        #insert the user into the database
+
+        cursor = db.cursor()
+        query = "INSERT INTO users (name,email,password) VALUES (%s, %s, %s)"
+        values = (name,email,password)
+        cursor.execute(query, values)
+        db.commit()
+
+        return "Registration succesful"
+
     return render_template('register.html')
 
 @app.route('/blog')
