6e8b0188cfe80f75d9e134029da26a7a90b02635
Joshua Franco <francoswim@yahoo.com>
2023-06-08 23:55:42 -0400

    Updating HTML and App.py

--- Broken_Access_Control/IDOR/app.py
+++ Broken_Access_Control/IDOR/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
